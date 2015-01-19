df223d1d3c90f9d4cc83a52ef4785538578a5526
Gabrielle <gcchwalik@ucdavis.edu>
2015-01-18 22:42:58 -0800

    implemented individual locks

---
+++ test.cpp
@@ -0,0 +1,31 @@
+// Ryan King
+// I ran this in the CSIF with the following compile command:
+// g++ -fopenmp -o P2.out Problem2.cpp
+
+#define TINY_MASK(x) (((u_int32_t)1<<(x))-1)
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+// We are using an unordered map as our hash table for this version
+#include <tr1/unordered_map>
+#include <string>
+#include <sstream>
+#include <iostream>
+#include <iterator>
+#include <ctime>
+#include <sys/time.h>
+#include <math.h>
+#include <stdint.h>
+#include <cstring>
+#include <iomanip>
+
+using namespace std;
+
+int main( int argc, const char* argv[] ) {
+
+  int many = row, smile, toss = 2;
+
+
+  return 0;
+}
